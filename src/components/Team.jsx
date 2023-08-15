2b11daa69a6a46fc9493f6e13304548d55ab1fac
Joel <ajiolajoel@gmail.com>
2023-08-15 08:22:25 +0100

    update to altruist

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -6,6 +6,7 @@ import {
     TbBrandTwitter,
     TbBrandInstagram,
   } from "react-icons/tb";
+  import image from "../assets/altruist-updated-logo-removebg-preview.png";
 
 const Team = () => {
   return (
@@ -14,6 +15,7 @@ const Team = () => {
           <h3 className='text-blue-700 font-bold text-2xl text-center'>Team</h3>
           <div className='flex justify-center gap-3'>
            <div className='flex flex-col bg-blue-500'>
+            <img src={image} alt="img-team" />
             <h3>{teamName.CEO}</h3>
             <p>{teamPost.first}</p>
             <div className='flex'>
@@ -24,6 +26,7 @@ const Team = () => {
             </div>
            </div>
            <div className='flex flex-col bg-blue-500'>
+            <img src={image} alt="img-team" />
             <h3>{teamName.contentCreator}</h3>
             <p>{teamPost.second}</p>
             <div className='flex'>
@@ -34,6 +37,7 @@ const Team = () => {
             </div>
            </div>
            <div className='flex flex-col bg-blue-500'>
+            <img src={image} alt="img-team" />
             <h3>{teamName.webDev}</h3>
             <p>{teamPost.third}</p>
             <div className='flex'>
