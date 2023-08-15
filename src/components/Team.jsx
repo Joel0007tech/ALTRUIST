9b0de61b68ba99fc392ef1534359eabc437fb4c8
Joel <ajiolajoel@gmail.com>
2023-08-15 07:45:16 +0100

    update to altruist

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import { teamName, teamPost } from '../constants'
+import {
+    TbBrandWhatsapp,
+    TbBrandLinkedin,
+    TbBrandTwitter,
+    TbBrandInstagram,
+  } from "react-icons/tb";
 
 const Team = () => {
   return (
-    <div>Team</div>
+    <div>
+        <div>
+          <h3 className='text-blue-700 font-bold text-2xl text-center'>Team</h3>
+          <div className='flex justify-center'>
+           <div className='flex flex-col'>
+            <h3></h3>
+            <p></p>
+            <div className='flex'>
+            <TbBrandInstagram/>
+            <TbBrandLinkedin/>
+            <TbBrandTwitter/>
+            <TbBrandWhatsapp/>
+            </div>
+           </div>
+          </div>
+        </div>
+    </div>
   )
 }
 
