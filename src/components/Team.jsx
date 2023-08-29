c3e528997667adc624913d618f55307e63ab99b1
Joel <ajiolajoel@gmail.com>
2023-08-29 08:09:48 +0100

    update to altruist

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -21,7 +21,7 @@ const Team = () => {
         <div>
           <h3 className='text-blue-700 font-bold text-2xl text-center'>Team</h3>
           <div className='flex justify-center gap-3 sm:flex sm:flex-col'>
-           <div className='flex flex-col bg-blue-500 p-16 sm:rounded-md sm:w-[350px] sm:m-auto'>
+           <div className='flex flex-col bg-blue-500 p-16 2xl:rounded-md sm:rounded-md sm:w-[350px] sm:m-auto'>
             <img src={Joybliss} alt="img-team" style={{width:200, height:250, borderRadius:400/2}} className='sm:m-auto'/>
             <h3 className="text-center text-white" style={Font}>{teamName.CEO}</h3>
             <p className="text-center text-white" style={Font}>{teamPost.first}</p>
@@ -32,7 +32,7 @@ const Team = () => {
             <TbBrandWhatsapp/>
             </div>
            </div>
-           <div className='flex flex-col bg-blue-500 p-16 sm:rounded-md sm:w-[350px] sm:m-auto'>
+           <div className='flex flex-col bg-blue-500 p-16 2xl:rounded-md sm:rounded-md sm:w-[350px] sm:m-auto'>
             <img src={image} alt="img-team" style={{width:200, height:250}}/>
             <h3 className="text-center text-white" style={Font}>{teamName.contentCreator}</h3>
             <p className="text-center text-white" style={Font}>{teamPost.second}</p>
@@ -43,7 +43,7 @@ const Team = () => {
             <TbBrandWhatsapp/>
             </div>
            </div>
-           <div className='flex flex-col bg-blue-500 pt-16 sm:pb-16 sm:rounded-md sm:w-[350px] sm:m-auto'>
+           <div className='flex flex-col bg-blue-500 pt-16 2xl:rounded-md sm:pb-16 sm:rounded-md sm:w-[350px] sm:m-auto'>
             <img src={Joel} alt="img-team" style={{width:320, height:250, borderRadius:400/2}} className='sm:m-auto'/>
             <h3 className="text-center text-white" style={Font}>{teamName.webDev}</h3>
             <p className="text-center text-white" style={Font}>{teamPost.third}</p>
